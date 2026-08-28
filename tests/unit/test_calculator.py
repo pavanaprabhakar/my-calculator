f909da52f1c75ac742f4c16fa2bbaf2f2a4aabda
pavanaprabhaakar <pavanaprabhakar05@gmail.com>
2026-08-28 13:59:18 +0530

    Added and Tested with negative values

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -34,3 +34,14 @@ class TestMultiplyDivideWithValidation:
             divide("10", 2)
 
 # TODO: Students will add TestMultiplyDivide class
+
+    def test_add_negative_numbers(self):
+        """Test adding negative numbers"""
+        assert add(-1, -1) == -2
+        assert add(-5, 3) == -2
+
+
+    def test_subtract_negative_numbers(self):
+        """Test subtracting negative numbers"""
+        assert subtract(-1, -1) == 0
+        assert subtract(-5, -3) == -2
